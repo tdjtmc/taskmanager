c2cb97aae2bef0a5d32e06e660d06c1a0718e14b
Tadija Tomić <ofah993@gmail.com>
2023-05-02 14:02:50 +0200

    init commit

---
+++ src/views/PendingTaskView.vue
@@ -0,0 +1,14 @@
+<template>
+  <task-input />
+  <task-list />
+</template>
+
+<script>
+// @ is an alias to /src
+import TaskInput from "../components/TaskInput.vue";
+import TaskList from "../components/TaskList.vue";
+export default {
+  name: "PendingTaskView",
+  components: { TaskInput, TaskList },
+};
+</script>
