3868e6447dd838ee3188fd309337aa98bb4d5236
Tadija Tomić <ofah993@gmail.com>
2023-05-04 15:43:21 +0200

    testing new view

--- src/views/CompletedTaskView.vue
+++ src/views/CompletedTaskView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <ul>
+      <li>Learn CSS</li>
+      <li>Learn HTML</li>
+      <li>Go to shopping</li>
+      <li>Sleep</li>
+      <li>Buy salt</li>
+    </ul>
   </div>
 </template>
+<style scoped>
+.container {
+  max-width: 750px;
+  margin: 0 auto;
+  color: #fafafa;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+  list-style: none;
+  width: 65%;
+  justify-content: space-between;
+}
+
+ul li {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  color: #2e1f1f;
+  border-bottom: 1px solid #42361f;
+}
+</style>
